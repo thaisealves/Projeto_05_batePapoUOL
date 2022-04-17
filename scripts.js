521be64f04ee43f3c32780b9a27f71a79ff45c80
thaisealves <alves.thaisesilva@gmail.com>
2022-04-17 04:28:17 -0300

    Bônus de inicialização implementado

--- scripts.js
+++ scripts.js
@@ -1,43 +1,41 @@
 let messages = []; // messages we're going to collect from the API are going to be here
-
 const participant = {
-    name: prompt("Qual o seu nomezinho?")
+    name: ''
 } // name I'm putting as mine, like, the participant one 
-
+let textEnter = document.querySelector("footer textarea")
+textEnter.addEventListener("keyup")
 function entering() {
+    participant.name = document.querySelector(".begin input").value;
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", participant);
-    promise.then(getMessages)// keep on showing messages at the time it appears, reloading the messages all the time
     promise.catch(error)
-} // loging into the page, like, putting my name in the participants area
-entering();
+    promise.then(getMessages)// keep on showing messages at the time it appears, reloading the messages all the time
 
-function keepStatus() {
-    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", participant);
-}   //see if I'm here, still logged and everything 
-setInterval(keepStatus, 4000);
+} // loging into the page, like, putting my name in the participants area
+
+
+function error(err) {
+    if (err.response.status === 400) {
+        alert("Nome já utilizado, você deve escolher outro!")
+        setTimeout(entering, 5000)
+    }
+} //if something goes wrong on the part of getting messages (username already used)
 
 function getMessages() {
+    document.querySelector(".begin").classList.add("hidden")
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
 
     promise.then(loadData);
 } // getting the messages from the API 
-setInterval(getMessages, 3000)// keep on showing messages at the time it appears, reloading the messages all the time
-
-
-function error(err) {
-    console.log(err.response);
-
-    if (err.response.status === 400) {
-        participant.name = prompt("Nome já utilizado, você deve escolher outro!")
-        entering()
-    }
-} //if something goes wrong on the part of getting messages (username already used)
 
 function loadData(response) {
     messages = response.data;
     printMessages();
 } // if everything goes well, we're putting the messages on our array **THE PROMISE HAS A RESPONSE
 
+function keepStatus() {
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", participant);
+}   //see if I'm here, still logged and everything 
+setInterval(keepStatus, 4000);
 
 //the messages appearing on the "app" 
 function printMessages() {
@@ -61,7 +59,7 @@ function printMessages() {
             else {
                 document.getElementById(`${i}`).classList.add("groupMsg")
             }
-           
+
         }
         if (messages[i].type === "private_message") {
             if (messages[i].to !== participant.name) {
@@ -77,8 +75,11 @@ function printMessages() {
         if (messages[i].type === "private_message") {
             document.getElementById(`${i}`).querySelector(".name").innerHTML += ` <span class='not-bold'> reservadamente para</span> ${messages[i].to}`
         }
+
     }
+
     pageScroll();
+    setInterval(getMessages, 4000);
 }
 function pageScroll() {
     window.scrollBy(0, 1);
@@ -94,10 +95,10 @@ function publicMessage() {
         type: "message"
     }
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", message);
-   text.value = ''; // here I can clear the input area, so I can write another thing
-   promise.then(getMessages());
-   promise.catch(function(){
-       alert("Desconectado da sala, vamos te conectar novamente")
-       window.location.reload();
-   })
+    text.value = ''; // here I can clear the input area, so I can write another thing
+    promise.then(getMessages());
+    promise.catch(function () {
+        alert("Desconectado da sala, vamos te conectar novamente")
+        window.location.reload();
+    })
 }
